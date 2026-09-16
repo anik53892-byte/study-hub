3f039d6d9e2489323af5bda4c1705169636bc590
anik53892-byte <anik53892@gmail.com>
2026-09-17 04:38:53 +0600

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,20 @@
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+  runtimeCaching: [
+    {
+      urlPattern: /^https?.*\.(?:js|css|woff2?|png|jpg|jpeg|svg|ico)$/,
+      handler: "StaleWhileRevalidate",
+      options: { cacheName: "static-assets" },
+    },
+  ],
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+module.exports = withPWA(nextConfig);
